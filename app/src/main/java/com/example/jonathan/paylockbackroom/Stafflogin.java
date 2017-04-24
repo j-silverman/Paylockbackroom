3e6246f7962c200daa4fa2069253b3fb8fce2784
j-silverman <jhs2018@bu.edu>
2017-04-24 19:45:34 -0400

    Implement staff login

--- app/src/main/java/com/example/jonathan/paylockbackroom/Stafflogin.java
+++ app/src/main/java/com/example/jonathan/paylockbackroom/Stafflogin.java
@@ -1,16 +1,34 @@
 package com.example.jonathan.paylockbackroom;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.util.DisplayMetrics;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 /**
  * Created by Jonathan on 4/7/2017.
  */
 public class Stafflogin extends AppCompatActivity implements View.OnClickListener{
-    private View view;
+    private Button bStaffLogin;
+    private EditText etStaffEmail;
+    private EditText etStaffPassword;
+
+    private FirebaseAuth firebaseAuth;
+
+    private ProgressDialog progressDialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -23,7 +41,45 @@ public class Stafflogin extends AppCompatActivity implements View.OnClickListene
         int height = dm.heightPixels;
 
         getWindow().setLayout((int)(width*.8),(int)(height*.4));
+        etStaffEmail = (EditText) findViewById(R.id.etStaffEmail);
+        etStaffPassword = (EditText) findViewById(R.id.etStaffPassword);
+        bStaffLogin = (Button) findViewById(R.id.bStaffLogin);
 
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        if(firebaseAuth.getCurrentUser()!= null){
+            finish();
+            startActivity(new Intent(getApplicationContext(),StaffUI.class));
+        }
+        progressDialog = new ProgressDialog(this);
+
+        bStaffLogin.setOnClickListener(this);
+    }
+    private void staffLogin(){
+        String email = etStaffEmail.getText().toString().trim();
+        String password = etStaffPassword.getText().toString().trim();
+        if(TextUtils.isEmpty(email)){
+            Toast.makeText(this,"Please enter Email", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(TextUtils.isEmpty(password)){
+            Toast.makeText(this,"Please enter Password", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        progressDialog.setMessage("Logging in...");
+        progressDialog.show();
+
+        firebaseAuth.signInWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            finish();
+                            startActivity(new Intent(getApplicationContext(),StaffUI.class));
+
+                        }
+                    }
+                });
     }
     //public void staffui(View view){
       //  this.view = view;
@@ -35,6 +91,8 @@ public class Stafflogin extends AppCompatActivity implements View.OnClickListene
 
     @Override
     public void onClick(View v) {
-
+        if(v == bStaffLogin){
+            staffLogin();
+        }
     }
 }
